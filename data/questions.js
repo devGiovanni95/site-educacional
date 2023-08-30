f9501685c77c1e70a2310a2c19f8765ce89bf480
Giovanni Almeida dos Santos <69327898+devGiovanni95@users.noreply.github.com>
2023-08-30 18:14:20 -0300

    Add files via upload

---
+++ data/questions.js
@@ -0,0 +1,46 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Qual filósofo criou a máquina de somar e multiplicar?",
+      answer: "Gottfried",
+      options: [
+        "Gottfried",
+        "Platão",
+        "Mark",
+        "Pascal",
+      ],
+    },
+    {
+      numb: 2,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "O alemão Konrad Zuse desenvolveu o Z-1, baseado no que ?",
+      answer: "Relés e lógica binária",
+      options: [
+        "Relés e lógica binária",
+        "Relés",
+        "Lógica binária",
+        "Cascading",
+      ],
+    },
+    {
+      numb: 3,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Quando se iniciou o desenvolvimento do linux ?",
+      answer: "1991",
+      options: [
+        "1991",
+        "1992",
+        "1993",
+        "1995",
+      ],
+    },
+  ];
+  
